c22f9c7ff9d858b6714994012f0143b3e4c1a5d5
btholmes <btholmes@iastate.edu>
2017-01-25 09:06:27 -0600

    added facebook login tutorial

---
+++ ScreenSketches/app/src/main/java/com/example/btholmes/screensketches/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.btholmes.screensketches;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
